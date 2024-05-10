f41fab512f01242ac94921cf93c52c0af14d9ba1
Hamidreza <www.hamidreza121212@gmail.com>
2024-05-10 19:31:37 +0330

    doc completed

--- python/importer/importer.py
+++ python/importer/importer.py
@@ -4,6 +4,7 @@ from kernel.settings.core.logs import configure_logging
 from painless.utils.funcs import create_directories
 from kernel.settings import config
 import logging
+import pandas as pd
 from my_elasticsearch import ElasticConnector
 
 coreLogger = logging.getLogger('core')
@@ -28,15 +29,109 @@ def main():
 
     # a = es_connector.create_index('country', body=mapping)
 
-    with open('C:/Users/Admin/Desktop/PROJECT/python_elasticsearch/python/importer/data/file_sources/country.csv', 'r') as file:
-        next(file)
-        for line in file:
-            fields = line.strip().split(',')
-            doc_id = fields[0]
-            document = {f'field_{idx}': value for idx,
-                        value in enumerate(fields[1:], start=1)}
-            es_connector.insert_document(
-                index_name='country', document=document)
+    # df = pd.read_csv(
+    #     'C:/Users/Admin/Desktop/PROJECT/python_elasticsearch/python/importer/data/file_sources/country.csv')
+
+    # for index, row in df.iterrows():
+    #     document = row.to_dict()
+    #     es_connector.insert_document(index_name='country', document=document)
+
+
+
+    # region : "Africa" 
+    query = {
+        "query": {
+            "match": {
+                "region": "Africa"
+
+            }
+        }
+    }
+
+    # region : "Europe"  or name : "Antigua and Barbuda" 
+    query = {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"region": "Europe"}},
+                    {"match": {"name": "Antigua and Barbuda"}}
+                ]
+            }
+        }
+
+    }
+
+    # region : "Europe" and name : "Jersey" 
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"region": "Europe"}},
+                    {"match": {"name": "Jersey"}}
+                ]
+            }
+        }
+    }
+
+    # region : "Cuba" or : "Cuba"
+    query = {
+        "query": {
+            "multi_match": {
+                "query": "Cuba",
+                "fields": ["name", "region"]
+            }
+        }
+    }
+
+
+    # query = {
+    #     "size": 0,
+    #     "aggs": {
+    #         "region_count": {
+    #             "terms": {
+    #                 "field": "region"
+    #             }
+    #         }
+    #     }
+    # }
+
+    # query = {
+    #     "size": 0,
+    #     "aggs": {
+    #         "region_count": {
+    #             "terms": {
+    #                 "field": "region"
+    #             },
+    #             "aggs": {
+    #                 "ava_region_code": {
+    #                     "avg": {
+    #                         "field": "sub-region-code"
+    #                     }
+    #                 },
+    #                 "max_region_code": {
+    #                     "max": {
+    #                         "field": "sub-region-code"
+    #                     }
+    #                 },
+    #                 "min_region_code": {
+    #                     "min": {
+    #                         "field": "sub-region-code"
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     }
+    # }
+
+
+
+    response = es_connector.search(index='country', body=query)
+
+    for hit in response['hits']['hits']:
+        print(hit['_source'])
+
+    # for hit in response['aggregations']['region_count']['buckets']:
+    #     print(hit)
 
 
 if __name__ == "__main__":
